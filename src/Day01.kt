44f0a315c895f602326591f97d1ecd437465f0ce
Marcel Joss <marceljoss@hotmail.ch>
2023-12-01 07:11:03 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,33 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Int = input.sumOf {
+        line -> line.first { it.isDigit() }.digitToInt() * 10 + line.last { it.isDigit() }.digitToInt()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    val digitMap = mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9,
+    )
+
+    fun String.tryToDigit(): Int? {
+        this[0].let { if (it.isDigit()) return it.digitToInt() }
+        return digitMap.entries.firstOrNull { startsWith(it.key) }?.value
+    }
+
+    fun part2(input: List<String>): Int = input.sumOf { line ->
+        val windows = line.windowed(digitMap.keys.maxOf { it.length }, partialWindows = true)
+        windows.firstNotNullOf { it.tryToDigit() } * 10 + windows.lastNotNullOf { it.tryToDigit() }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(readInput("Day01_test")) == 142)
+    check(part2(readInput("Day01_test2")) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
